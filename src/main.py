e28d09cf8b46faec2534f90bbe9f4f807f2ff2b2
Daniel Barbosa Rocha <daniel.dr206@gmail.com>
2023-12-04 20:33:19 -0300

    feat: camera rotation

--- src/main.py
+++ src/main.py
@@ -6,12 +6,21 @@ import math
 import random
 import time
 import ctypes
+import numpy as np
 from components.target import draw_arena
 
-# Variáveis globais para controlar a posição da câmera
-camera_position = [0, 0, 5]
-look_at = [0, 0, 0]
-up_vector = [0, 1, 0]
+# Posição da câmera
+camera_pos = [0.0, 0.0, 5.0]
+camera_angle = 0.0
+
+# Direção para onde a câmera está olhando
+camera_lookat = [0.0, 0.0, 0.0]
+
+# Vetor de 'up'
+camera_up = [0.0, 1.0, 0.0]
+
+# Ângulo de rotação inicial
+
 
 # Variáveis globais para controlar a posição do objeto
 object_position = [0, 0, 0]
@@ -22,14 +31,21 @@ def draw():
     glLoadIdentity()
 
     gluLookAt(
-        camera_position[0], camera_position[1], camera_position[2],
-        look_at[0], look_at[1], look_at[2],
-        up_vector[0], up_vector[1], up_vector[2]
+        camera_pos[0], camera_pos[1], camera_pos[2],
+        camera_lookat[0], camera_lookat[1], camera_lookat[2],
+        camera_up[0], camera_up[1], camera_up[2]
     )
 
     glColor3f(1.0, 1.0, 1.0)  # Cor do objeto (branco)
     glTranslatef(*object_position)
     draw_arena(0.5,0, 0, 0)
+
+    glPushMatrix()
+    glColor3f(1, 0, 0)
+    glTranslatef(camera_lookat[0],camera_lookat[1], camera_lookat[2])
+    glScalef(0.1, 0.1, 0.1)
+    glutWireCube(2)
+    glPopMatrix()
 
 
 
@@ -50,57 +66,41 @@ def mouse_click(button, state, x, y):
 
 
 def special_key_pressed(key, x, y):
-    global camera_position, object_position
-
-
-    # Atualiza a posição da câmera ou do objeto com base na tecla pressionada
-
-    if look_at[2] > 5:
-        look_at[2] = 5
-
-    if look_at[0] < -5:
-        look_at[0] = -5
-
-    if look_at[0] > 5:
-        look_at[0] = 5
-
+    global camera_pos, object_position,camera_angle, camera_lookat
     
-    if key == GLUT_KEY_DOWN:
-        if(look_at[1] > -1):
-            look_at[1] -= 0.1
-        else:
-            look_at[1] = -1
+    # if key == GLUT_KEY_DOWN:
+    #     if(look_at[1] > -1):
+    #         look_at[1] -= 0.1
+    #     else:
+    #         look_at[1] = -1
 
-    elif key == GLUT_KEY_UP:
-        if(look_at[1] < 2):
-            look_at[1] += 0.1
-        else:
-            look_at[1] = 2
+    # elif key == GLUT_KEY_UP:
+    #     if(look_at[1] < 2):
+    #         look_at[1] += 0.1
+    #     else:
+    #         look_at[1] = 2
 
-    elif key == GLUT_KEY_LEFT:
-            if(look_at[2] <= 5):
-                look_at[0] -= math.cos(1.55)*5
-                if(look_at[0] < 0):
-                    look_at[2] += math.sin(0.01745)*5
-                else:
-                    look_at[2] -= math.sin(0.01745)*5
+    if key == GLUT_KEY_LEFT:
+        camera_angle += 5.0
+        camera_lookat[0] = -math.sin(math.radians(camera_angle))*5
+        camera_lookat[2] = 5-math.cos(math.radians(camera_angle))*5
+        if camera_angle >= 360.0:
+            camera_angle -= 360.0
             
 
     elif key == GLUT_KEY_RIGHT:
-            if(look_at[2] <= 5):
-                look_at[0] += math.cos(1.55)*5
-                if(look_at[0] < 0): 
-                    look_at[2] -= math.sin(0.01745)*5
-                else:
-                    look_at[2] += math.sin(0.01745)*5
+        camera_angle -= 5.0
+        camera_lookat[0] = -math.sin(math.radians(camera_angle))*5
+        camera_lookat[2] = 5-math.cos(math.radians(camera_angle))*5
+        if camera_angle <= 0.0:
+            camera_angle += 360.0
             
                     
 
 
 
-    print("Look at | X: " + str(look_at[0]) + " Y: " + str(look_at[1]) + " Z: " + str(look_at[2]))
-    print("Camera  | X: " + str(camera_position[0]) + " Y: " + str(camera_position[1]) + " Z: " + str(camera_position[2]))
-
+    print("Look at | X: " + str(camera_lookat[0]) + " Y: " + str(camera_lookat[1]) + " Z: " + str(camera_lookat[2]))
+    print("angle: " + str(camera_angle))
     glutPostRedisplay()
 
 
